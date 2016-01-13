5197953ad4fff50cb1bd589d1da611e4e8036984
Philip Silva <psilva.de@gmail.com>
2016-01-13 18:29:23 +0100

    Ping: return errors

--- xmpp_ping.go
+++ xmpp_ping.go
@@ -4,16 +4,18 @@ import (
 	"fmt"
 )
 
-func (c *Client) PingC2S(jid, server string) {
-	fmt.Fprintf(c.conn, "<iq from='%s' to='%s' id='c2s1' type='get'>\n"+
+func (c *Client) PingC2S(jid, server string) error {
+	_, err := fmt.Fprintf(c.conn, "<iq from='%s' to='%s' id='c2s1' type='get'>\n"+
 		"<ping xmlns='urn:xmpp:ping'/>\n"+
 		"</iq>",
 		xmlEscape(jid), xmlEscape(server))
+	return err
 }
 
-func (c *Client) PingS2S(fromServer, toServer string) {
-	fmt.Fprintf(c.conn, "<iq from='%s' to='%s' id='s2s1' type='get'>\n"+
+func (c *Client) PingS2S(fromServer, toServer string) error {
+	_, err := fmt.Fprintf(c.conn, "<iq from='%s' to='%s' id='s2s1' type='get'>\n"+
 		"<ping xmlns='urn:xmpp:ping'/>\n"+
 		"</iq>",
 		xmlEscape(fromServer), xmlEscape(toServer))
+	return err
 }
