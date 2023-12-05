a67513080ca186009fc14b1d1ece20b3f00d34d5
wuriyanto <wuriyanto48@yahoo.co.id>
2023-12-05 13:17:22 +0700

    fix example: the DefaultConfig should use the tls.Config pointer

--- _example/example.go
+++ _example/example.go
@@ -40,7 +40,7 @@ func main() {
 	}
 
 	if !*notls {
-		xmpp.DefaultConfig = tls.Config{
+		xmpp.DefaultConfig = &tls.Config{
 			ServerName:         serverName(*server),
 			InsecureSkipVerify: false,
 		}
