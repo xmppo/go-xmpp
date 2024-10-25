b9f59def0f821bbf740615cdf8b436bda4ca4dd2
Martin Dosch <martin@mdosch.de>
2024-10-26 00:45:21 +0200

    Start new development cycle.

--- xmpp.go
+++ xmpp.go
@@ -57,7 +57,7 @@ const (
 	nsClient       = "jabber:client"
 	nsSession      = "urn:ietf:params:xml:ns:xmpp-session"
 	nsStreamLimits = "urn:xmpp:stream-limits:0"
-	Version        = "0.2.4"
+	Version        = "0.2.5-dev"
 )
 
 // Default TLS configuration options
