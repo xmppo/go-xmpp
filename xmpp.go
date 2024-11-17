af179f2a1f2139b916aa6e7d97d729e67538071a
Martin Dosch <martin@mdosch.de>
2024-11-17 12:02:49 +0100

    Revert "Start new development cycle."
    This reverts commit a2eb99afa26eb50e62eebd1cf0f3186d56b8c69c.

--- xmpp.go
+++ xmpp.go
@@ -46,7 +46,7 @@ import (
 )
 
 const (
-	Version         = "0.2.7-dev"
+	Version         = "0.2.6"
 	nsStream        = "http://etherx.jabber.org/streams"
 	nsTLS           = "urn:ietf:params:xml:ns:xmpp-tls"
 	nsSASL          = "urn:ietf:params:xml:ns:xmpp-sasl"
