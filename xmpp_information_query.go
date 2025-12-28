37761f4d85628aec3107e497ae694cb8056a3859
eleksir <eleksir@exs-elm.ru>
2025-12-28 14:12:45 +0300

    Fix linter complaints in xmpp_information_query.go

--- xmpp_information_query.go
+++ xmpp_information_query.go
@@ -5,53 +5,84 @@ import (
 	"time"
 )
 
+// Discovery discovers items information according https://xmpp.org/extensions/xep-0030.html#items (Discovering the
+// Items Associated with a Jabber Entity).
 func (c *Client) Discovery() (string, error) {
-	// use UUIDv4 for a pseudo random id.
-	reqID := getUUID()
-	return c.RawInformationQuery(c.jid, c.domain, reqID, IQTypeGet, XMPPNS_DISCO_ITEMS, "")
+	return c.RawInformationQuery(c.jid, c.domain, getUUID(), IQTypeGet, XMPPNS_DISCO_ITEMS, "")
 }
 
-// Discover information about a node. Empty node queries info about server itself.
+// DiscoverNodeInfo discovers information about a node. Empty node queries info about server itself.
+// Discovery query performed according to https://xmpp.org/extensions/xep-0030.html#info (Discovering Information About
+// a Jabber Entity).
 func (c *Client) DiscoverNodeInfo(node string) (string, error) {
-	query := fmt.Sprintf("<query xmlns='%s' node='%s'/>", XMPPNS_DISCO_INFO, node)
-	return c.RawInformation(c.jid, c.domain, getUUID(), IQTypeGet, query)
+	return c.RawInformation(
+		c.jid,
+		c.domain,
+		getUUID(),
+		IQTypeGet,
+		fmt.Sprintf("<query xmlns=%q node=%q/>", XMPPNS_DISCO_INFO, node),
+	)
 }
 
-// Discover information about given item from given jid.
+// DiscoverInfo discovers information about given item from given jid.
+// Discovery query performed according to https://xmpp.org/extensions/xep-0030.html#info (Discovering Information About
+// a Jabber Entity).
+// The only difference between DiscoverInfo() and DiscoverNodeInfo() is that DiscoverInfo() does not supply From field,
+// which is useful in very limited amount use cases.
 func (c *Client) DiscoverInfo(to string) (string, error) {
-	query := fmt.Sprintf("<query xmlns='%s'/>", XMPPNS_DISCO_INFO)
+	query := fmt.Sprintf("<query xmlns=%q/>", XMPPNS_DISCO_INFO)
+
 	return c.RawInformation(c.jid, to, getUUID(), IQTypeGet, query)
 }
 
-// Discover items that the server exposes
+// DiscoverServerItems discovers items that the server exposes. It is actually thin wrapper for DiscoverEntityItems().
 func (c *Client) DiscoverServerItems() (string, error) {
 	return c.DiscoverEntityItems(c.domain)
 }
 
-// Discover items that an entity exposes
+// DiscoverEntityItems discovers items that an entity exposes.
 func (c *Client) DiscoverEntityItems(jid string) (string, error) {
-	query := fmt.Sprintf("<query xmlns='%s'/>", XMPPNS_DISCO_ITEMS)
+	query := fmt.Sprintf("<query xmlns=%q/>", XMPPNS_DISCO_ITEMS)
+
 	return c.RawInformation(c.jid, jid, getUUID(), IQTypeGet, query)
 }
 
 // RawInformationQuery sends an information query request to the server.
 func (c *Client) RawInformationQuery(from, to, id, iqType, requestNamespace, body string) (string, error) {
-	const xmlIQ = "<iq from='%s' to='%s' id='%s' type='%s'><query xmlns='%s'>%s</query></iq>\n"
-	_, err := fmt.Fprintf(c.stanzaWriter, xmlIQ, xmlEscape(from), xmlEscape(to), id, iqType, requestNamespace, body)
+	_, err := fmt.Fprintf(
+		c.stanzaWriter,
+		"<iq from=%q to=%q id=%q type=%q><query xmlns=%q>%s</query></iq>\n",
+		xmlEscape(from),
+		xmlEscape(to),
+		id,
+		iqType,
+		requestNamespace,
+		body,
+	)
+
 	return id, err
 }
 
-// rawInformation send a IQ request with the payload body to the server
+// RawInformation send a IQ request with the payload body to the server.
 func (c *Client) RawInformation(from, to, id, iqType, body string) (string, error) {
-	const xmlIQ = "<iq from='%s' to='%s' id='%s' type='%s'>%s</iq>\n"
-	_, err := fmt.Fprintf(c.stanzaWriter, xmlIQ, xmlEscape(from), xmlEscape(to), id, iqType, body)
+	_, err := fmt.Fprintf(
+		c.stanzaWriter,
+		"<iq from=%q to=%q id=%q type=%q>%s</iq>\n",
+		xmlEscape(from),
+		xmlEscape(to),
+		id,
+		iqType,
+		body,
+	)
+
 	return id, err
 }
 
-// UrnXMPPTimeResponse implements response to query entity's current time (xep-0202).
+// UrnXMPPTimeResponse implements response to query entity's current time accodring to
+// https://xmpp.org/extensions/xep-0202.html#example-2 (A Response to the Query).
 func (c *Client) UrnXMPPTimeResponse(v IQ, timezoneOffset string) (string, error) {
 	query := fmt.Sprintf(
-		"<time xmlns=\"%s\"><tzo>%s</tzo><utc>%s</utc></time>",
+		"<time xmlns=%q><tzo>%s</tzo><utc>%s</utc></time>",
 		XMPPNS_TIME,
 		timezoneOffset,
 		time.Now().UTC().Format(time.RFC3339),
@@ -66,7 +97,8 @@ func (c *Client) UrnXMPPTimeResponse(v IQ, timezoneOffset string) (string, error
 	)
 }
 
-// IqVersionResponse responding with software version, according to xep-0092.
+// IqVersionResponse responding with software version, according to example described in
+// https://xmpp.org/extensions/xep-0092.html#example-2 (Receiving a Reply Regarding Software Version).
 func (c *Client) IqVersionResponse(v IQ, name string, version string, os string) (string, error) {
 	if name == "" {
 		name = "go-xmpp"
@@ -77,7 +109,8 @@ func (c *Client) IqVersionResponse(v IQ, name string, version string, os string)
 		version = "undefined"
 	}
 
-	query := fmt.Sprintf("<query xmlns=\"%s\">", XMPPNS_IQ_VERSION)
+	query := fmt.Sprintf("<query xmlns=%q>", XMPPNS_IQ_VERSION)
+
 	query += fmt.Sprintf("<name>%s</name>", name)
 	query += fmt.Sprintf("<version>%s</version>", version)
 
